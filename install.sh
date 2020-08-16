24f3af679930d8b9af4ae8e7d45c2c1c5ef50996
wansong <betterofsong@gmail.com>
2020-08-16 19:56:45 +0800

    move install_file

--- install.sh
+++ install.sh
@@ -21,44 +21,7 @@ done
 this_dir="$(dirname "$this_file")"
 
 
-install_file() {
-  local from_file=$1
-  if ! [[ "$from_file" =~ ^/ ]] ; then
-    from_file="$(pwd)/$from_file"
-  fi
-  local to_file=$2
-  mkdir -p "$(dirname "$to_file")"
-  backup_file "$to_file"
-  if ! ln -s "$from_file" "$to_file" ; then
-    echo "Failed to create symbolic link from $from_file to ${to_file}: maybe parent dir not exist or no permission to do so"
-  fi
-}
-
-backup_file() {
-  local ff=$1
-  if ! [ -e "$ff" ] ; then
-    return 0
-  fi
-
-  local backup_ff=${ff}.bak
-  if ! [ -f "$backup_ff" ] ; then
-    mv "$ff" "$backup_ff"
-    return 0
-  fi
-
-  local suffix=1
-  while [ -f "${backup_ff}.${suffix}" ] ; do
-    ((suffix++))
-  done
-
-  mv "$ff" "${backup_ff}.${suffix}"
-}
-
-# generate coc_config
-"${this_dir}/install_coc_config.sh"
-
 install_file "${this_dir}/gitconfig" "$HOME/.gitconfig"
-install_file "${this_dir}/coc_config.json" "$HOME/.vim/coc-settings.json" 
 install_file "${this_dir}/.vimrc" "$HOME/.vimrc" 
 install_file "${this_dir}/.xvimrc" "$HOME/.xvimrc" 
 install_file "${this_dir}/.bashrc" "$HOME/.bashrc" 
